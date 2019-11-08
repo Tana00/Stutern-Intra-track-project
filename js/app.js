612ff91c69f57d2b8347b978f1d13e5ee1b44b17
calistus <howell2028@yahoo.com>
2019-11-08 21:56:09 +0100

    Giving the navbar a menu on device view

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-new WOW().init();
+// new WOW().init();
 
-document.querySelector('activetext')
+// document.querySelector('activetext')
 
-switch (new Date().getDay()) {
-	default: 
-	console.log('you there?')
-	  text = 'Looking forward to the Weekend';
-	  break;
-	case 1:
-	  text = "Today is Saturday";
-	  break; 
-	case 0:
-	  text = "Today is Sunday";
-  }
+// switch (new Date().getDay()) {
+// 	default: 
+// 	console.log('you there?')
+// 	  text = 'Looking forward to the Weekend';
+// 	  break;
+// 	case 1:
+// 	  text = "Today is Saturday";
+// 	  break; 
+// 	case 0:
+// 	  text = "Today is Sunday";
+//   }
